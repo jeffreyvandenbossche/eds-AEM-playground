baa09f1188f6e4e91997206a91e1264ac5839df6
Jeffrey Vandenbossche <jeffrey.vandenbossche@gmail.com>
2025-04-04 13:23:11 +0200

    update script lint

--- scripts/update-references.js
+++ scripts/update-references.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 function updateReferences(directory) {
   const files = fs.readdirSync(directory);
-  
-  files.forEach(file => {
+
+  files.forEach((file) => {
     const filePath = path.join(directory, file);
     if (fs.statSync(filePath).isDirectory()) {
       updateReferences(filePath);
@@ -18,4 +18,3 @@ function updateReferences(directory) {
 }
 
 updateReferences('.');
-console.log('References updated successfully.');
